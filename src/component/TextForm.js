1cc26fc55cd68194071ff14835609954fd4cd38d
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2022-08-15 16:57:00 +0530

    Update TextForm.js

--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -1,21 +1,17 @@
 import React, { useState } from "react";
-// import PropTypes from "prop-types";
 
 export default function TextForm(props) {
   const [text, setText] = useState("");
   const UpperCaseClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
-    props.showAlert("Coverted to UPPERCASE", "success");
   };
   const LowerCaseClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
-    props.showAlert("Coverted to lowercase", "success");
   };
   const ClearText = () => {
     setText("");
-    props.showAlert("Text CLEARED", "danger");
   };
   const onchange = (event) => {
     setText(event.target.value);
@@ -24,66 +20,70 @@ export default function TextForm(props) {
     var text = document.getElementById("floatingTextarea");
     text.select();
     navigator.clipboard.writeText(text.value);
-    props.showAlert("Text Copied to Clipboard", "success");
   };
   return (
     <>
-    <div className="container">
-      <div style={{ color: props.mode === "dark" ? "white" : "#161620" }}>
-        <h1 className="my-3">Enter Your Text Below</h1>
-        <div className="mb-3">
-          <textarea
-            className="form-control  my-2"
-            value={text}
-            onChange={onchange}
-            rows="8"
-            style={{
-              
-              backgroundColor: props.mode === "dark" ? "#161620" : "white",
-              color: props.mode === "dark" ? "white" : "#161620",
-            }}
-            id="floatingTextarea"
+      <div className="container w-9/12 p-6 ">
+        <div style={{ color: props.mode === "dark" ? "white" : "#161620" }}>
+          <h1 className="mb-3 text-2xl font-semibold">
+            Enter Your Text Below :
+          </h1>
+          <div className="mb-3">
+            <textarea
+              className="form-control "
+              value={text}
+              onChange={onchange}
+              rows="8"
+              style={{
+                backgroundColor: props.mode === "dark" ? "#161620" : "white",
+                color: props.mode === "dark" ? "white" : "#161620",
+              }}
+              id="floatingTextarea"
+            >
+              {" "}
+            </textarea>
+          </div>
+          <button
+            className="btn btn-primary mx-2 w-2/12 "
+            onClick={UpperCaseClick}
           >
-            {" "}
-          </textarea>
+            Convert to UpperCase
+          </button>
+          <button
+            className="btn btn-primary mx-2 w-2/12"
+            onClick={LowerCaseClick}
+          >
+            Convert to LowerCase
+          </button>
+          <button className="btn btn-danger mx-2 w-2/12 " onClick={ClearText}>
+            Clear The Screen
+          </button>
+          <button className="btn btn-success mx-2 w-2/12 " onClick={CopyText}>
+            Copy Text
+          </button>
         </div>
-        <button className="btn btn-primary mx-1" onClick={UpperCaseClick}>
-          Convert to UpperCase
-        </button>
-        <button className="btn btn-primary mx-1" onClick={LowerCaseClick}>
-          Convert to LowerCase
-        </button>
-        <button className="btn btn-danger mx-1" onClick={ClearText}>
-          Clear The Screen
-        </button>
-        <button className="btn btn-success mx-1" onClick={CopyText}>
-          Copy Text
-        </button>
-      </div>
 
-      <div
-        style={{ color: props.mode === "dark" ? "white" : "#161620" }}
-      >
-        <h2 className="my-3">Text Summary</h2>
-        <p className="my-2">
-          <b>{text.split(" ").length}</b> Words and <b> {text.length}</b>{" "}
-          Characters
+        <div style={{ color: props.mode === "dark" ? "white" : "#161620" }}>
+          <h2 className="my-3 text-2xl font-semibold">Text Summary : </h2>
+          <p className=" mb-8 ">
+            <b>{text.split(" ").length - 1}</b> Words and <b> {text.length}</b>{" "}
+            Characters
+          </p>
+        </div>
+
+        <h2
+          className=" text-2xl mb-3 font-semibold"
+          style={{ color: props.mode === "dark" ? "white" : "#161620" }}
+        >
+          Preview Your Text Here :
+        </h2>
+        <p
+          className="my-2"
+          style={{ color: props.mode === "dark" ? "white" : "#161620" }}
+        >
+          {text.length > 0 ? text : "Enter something To preview"}
         </p>
       </div>
-      
-      <h2
-        className="my-3 "
-        style={{ color: props.mode === "dark" ? "white" : "#161620" }}>
-        Preview Your Text Here:
-      </h2>
-      <p
-        className="my-2"
-        style={{ color: props.mode === "dark" ? "white" : "#161620" }}
-      >
-        {text.length > 0 ? text : "Enter something To preview"}
-      </p>
-      </div>
-
     </>
   );
 }
